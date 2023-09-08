ecc541b7f68cb652d28fbe56fc706a946a59a067
MateuszSzczukowski <mateusz.szczukowski@ttc.com>
2023-09-08 15:19:46 +0200

    Created ProductList component and called it

---
+++ code/src/app/ui/components/organisms/ProductList.tsx
@@ -0,0 +1,18 @@
+import { ProductItem } from "../molecules/ProductItem";
+import { type ProductItemPropsType } from "@/app/types/ProductItem.types";
+
+export const ProductList = ({
+	products,
+}: {
+	products: ProductItemPropsType[];
+}) => {
+	return (
+		<ul className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+			{products.map((productItem) => (
+				<li className="group relative" key={productItem.id}>
+					<ProductItem {...productItem} />
+				</li>
+			))}
+		</ul>
+	);
+};
