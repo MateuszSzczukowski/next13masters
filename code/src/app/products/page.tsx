b94678529f2b054b65b772a46f7a7bd60d41dc1f
MateuszSzczukowski <mateusz.szczukowski@ttc.com>
2023-09-11 18:50:11 +0200

    Added a new products page

---
+++ code/src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { ProductList } from "@/ui/components/organisms/ProductList";
+import { productList } from "@/json/productList";
+
+export default function Products() {
+	return (
+		<>
+			<main className="flex min-h-screen flex-col items-center justify-between bg-white p-24">
+				<ProductList products={productList} />
+			</main>
+		</>
+	);
+}
